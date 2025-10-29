731b4516694a084b5b41b0f4144cbc679f5fd126
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-10-29 22:12:48 +0530

    Banner added

---
+++ Frontend/src/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+
+      
+        <div className="w-full py-2.5 font-medium text-sm text-white text-center bg-gradient-to-r from-violet-500 via-[#9938CA] to-[#E0724A]">
+            <p>Special Deal: New AI Features Available Now!</p>
+        </div>
+    
+   
+  )
+}
+
+export default Banner
