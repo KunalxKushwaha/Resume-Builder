80c68f92eb613ed52a1285e82b7d6c47cb75efeb
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-07 18:29:36 +0530

    Making Navbar

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,28 +1,33 @@
-import { Link, useNavigate } from 'lucide-react';
-import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
 
 const Navbar = () => {
-    const user = {name: "John Doe"};
-    const navigate = useNavigate();
-    const logoutUser = () => {
-        navigate('/');
-    }
+  const user = { name: "John Doe" };
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    navigate('/');
+  };
+
   return (
-    <div className='shadow bg-white'>
-        <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
-            <Link to='/'>
-            <img src="/Logo3.png" alt="logo" className='h-11 w-auto' />
-            </Link>
-            <div className='flex items-center gap-4 text-sm'>
-                <p className='max-sm:hidden'>Hi, {user?.name}</p>
-                <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>Logout 
+    <div className="shadow bg-white">
+      <nav className="flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all">
+        <Link to="/">
+          <img src="/Logo3.png" alt="logo" className="h-11 w-auto" />
+        </Link>
 
-                </button>
-            </div>
-        </nav>
-      
+        <div className="flex items-center gap-4 text-sm">
+          <p className="max-sm:hidden">Hi, {user?.name}</p>
+          <button
+            onClick={logoutUser}
+            className="bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all"
+          >
+            Logout
+          </button>
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
