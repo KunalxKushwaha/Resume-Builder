e2a4ec0b45d133f2a6b65ba4460b349980f8eeb6
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-07 18:22:40 +0530

    Making Navbzr

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link, useNavigate } from 'lucide-react';
+import React from 'react'
+
+const Navbar = () => {
+    const user = {name: "John Doe"};
+    const navigate = useNavigate();
+    const logoutUser = () => {
+        navigate('/');
+    }
+  return (
+    <div className='shadow bg-white'>
+        <nav className='flex items-center justify-between max-w-7xl mx-auto px-4 py-3.5 text-slate-800 transition-all'>
+            <Link to='/'>
+            <img src="/Logo3.png" alt="logo" className='h-11 w-auto' />
+            </Link>
+            <div>
+                <p>Hi, {user?.name}</p>
+                <button onClick={logoutUser} className='bg-white hover:bg-slate-50 border border-gray-300 px-7 py-1.5 rounded-full active:scale-95 transition-all'>Logout 
+
+                </button>
+            </div>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
