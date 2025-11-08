b087c06560768b2d0bc8c4328a2f0ada684abb2c
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-08 14:47:46 +0530

    Making Dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -81,7 +81,7 @@ const Dashboard = () => {
           <form onSubmit={createResume} onClick={()=> setShowCreateResume(false)} className='fixed inset-0 bg-black/70 backdrop-blur bg-opacity-50 z-10 flex items-center justify-center'>
             <div className='relative bg-slate-50 border shadow-md rounded-lg w-full max-w-sm p-6' onClick={e=> e.stopPropagation()}>
               <h2 className='text-xl font-bold mb-4'>Create a Resume</h2>
-              <input type="text" placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-puprle-600 ring-purple-600' required/>
+              <input onChange={(e)=> setTitle(e.target.value)} value={title} type="text" placeholder='Enter resume title' className='w-full px-4 py-2 mb-4 focus:border-puprle-600 ring-purple-600' required/>
               <button className='w-full py-2 bg-purple-600 text-white rounded hover:bg-puprle-700 transition-colors'>Create Resume</button>
               <XIcon className='absolute top-4 right-4 text-slate-400 hover:text-slate-600 cursor-pointer transition-colors' onClick={()=> {
                 setShowCreateResume(false); setTitle('')
