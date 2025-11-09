df059ea4b22a1dad5ee1a67c262c8fc8749d58b7
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-09 19:57:40 +0530

    Making Builder Page

--- Frontend/src/pages/ResumeBuilder.jsx
+++ Frontend/src/pages/ResumeBuilder.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const ResumeBuilder = () => {
+
+  const [resumeData, setResumeData] = React.useState({
+    _id: '',
+    title: '',
+    personal_info: {},
+    experience: [],
+    education: [],
+    projects: [],
+    skills: [],
+    template: "classic",
+    accent_color: '#3B82F6',
+    public: false,
+
+
+
+  })
   return (
     <div>
-      Resume Builder Page
+      
     </div>
   )
 }
