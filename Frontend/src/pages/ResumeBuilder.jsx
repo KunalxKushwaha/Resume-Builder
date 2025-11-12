b429409f39abdec7f214aab485ab855ed347ba84
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-12 21:00:29 +0530

    Making Experience Section

--- Frontend/src/pages/ResumeBuilder.jsx
+++ Frontend/src/pages/ResumeBuilder.jsx
@@ -7,6 +7,7 @@ import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
 import ColorPicker from '../components/ColorPicker'
 import SummaryForm from '../components/SummaryForm'
+import ExperienceForm from '../components/ExperienceForm'
 
 const ResumeBuilder = () => {
 
@@ -139,6 +140,13 @@ const ResumeBuilder = () => {
                    <SummaryForm data={resumeData.professional_summary} onChange={(data)=> setResumeData(prev=> ({...prev, professional_summary: data}))} setResumeData={setResumeData}/> 
                   )
                 }
+
+                {
+                  activeSection.id === 'experience' && (
+                   <ExperienceForm data={resumeData.experience} onChange={(data)=> setResumeData(prev=> ({...prev, experience: data}))} /> 
+                  )
+                }
+
 
               </div>
 
