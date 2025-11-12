2eaed54d579d51795d2288b0f6423ee75a395ef6
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-12 10:38:39 +0530

    Making Summary

--- Frontend/src/components/SummaryForm.jsx
+++ Frontend/src/components/SummaryForm.jsx
@@ -1,8 +1,25 @@
+import { Sparkle } from 'lucide-react'
 import React from 'react'
 
-const SummaryForm = () => {
+const SummaryForm = ({data, onChange, setResumeData}) => {
   return (
-    <div>
+    <div className='space-y-4'>
+        <div className='flex items-center justify-between'>
+            {/* Left Side */}
+            <div>
+                <h3 className='flex items-center gap-2 text-lg font-semibold text-gray-900'>Professional Summary</h3>
+                <p className='text-sm text-gray-500'>Add Summary for Yuor Resume Here</p>
+            </div>
+            {/* Right Side */}
+            <button className='flex items-center gap-2 px-3 py-1 text-sm bg-purple-100 text-purple-700 rounded hover:bg-purple-200 transition-colors disabled:opacity-50'>
+                <Sparkle className='size-4' /> AI Enhance
+            </button>
+
+        </div>
+
+        <div className='mt-6'>
+            <textarea className='w-full p-3 px-4 mt-2 border text-sm border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors resize-none' placeholder='Write a Compelling Professional Summary that highlights your key strengths and career objectives..'/>
+        </div>
       
     </div>
   )
