d60baf5342ebb3228eba2961994cb3d5dc1a45ab
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-12 13:18:12 +0530

    Making Summary

--- Frontend/src/components/SummaryForm.jsx
+++ Frontend/src/components/SummaryForm.jsx
@@ -18,7 +18,8 @@ const SummaryForm = ({data, onChange, setResumeData}) => {
         </div>
 
         <div className='mt-6'>
-            <textarea className='w-full p-3 px-4 mt-2 border text-sm border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors resize-none' placeholder='Write a Compelling Professional Summary that highlights your key strengths and career objectives..'/>
+            <textarea className='w-full p-3 px-4 mt-2 border text-sm border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500 outline-none transition-colors resize-none' placeholder='Write a Compelling Professional Summary that highlights your key strengths and career objectives..' rows={7} onChange={(e)=> onChange(e.target.value)} value={data || "" }/>
+                <p className='text-xs text-gray-500 max-w-4/5 mx-auto text-center'>Tip: Keep it Concise (3-4 Sentences) and focus on your Most Relevant Achievements and Skills.</p>
         </div>
       
     </div>
