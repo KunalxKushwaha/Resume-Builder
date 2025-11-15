747b2772c8f30f4eda71846ad43b187a8c16f25b
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-15 16:15:26 +0530

    Making the final buttons

--- Frontend/src/pages/ResumeBuilder.jsx
+++ Frontend/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { Link , useParams } from 'react-router-dom'
 import { dummyResumeData } from '../assets/assets'
-import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, FileText, FolderIcon, GraduationCap, Share2Icon, Sparkles, User } from 'lucide-react'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, ChevronRight, DownloadIcon, EyeIcon, EyeOff, FileText, FolderIcon, GraduationCap, Share2Icon, Sparkles, User } from 'lucide-react'
 import PersonalInfoForm from '../components/PersonalInfoForm'
 import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
@@ -184,10 +184,19 @@ const ResumeBuilder = () => {
               {/* Buttons */}
               <div className='absolute bottom-3 left-0 right-0 flex items-center justify-end gap-2'>
                 {resumeData.public && (
-                  <button>
+                  <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-blue-100 to-blue-200 text-blue-600 rounded-lg ring-blue-300 hover:ring transition-colors'>
                     <Share2Icon className='size-4'/>
                   </button>
                 )}
+                <button className='flex items-center p-2 px-4 gap-2 text-xs bg-gradient-to-br from-purple-100 to-purple-200 text-purple-600 rounded-lg ring-purple-300 hover:ring transition-colors'>
+                  {resumeData.public ? <EyeIcon className='size-4'/> : <EyeOff className='size-4'/>}
+                  {resumeData.public ? 'Public' : 'Private'}
+                </button>
+
+                <button className='flex items-center p-2 px-6 gap-2 text-xs bg-gradient-to-br from-green-100 to-green-200 text-green-600 rounded-lg ring-green-300 hover:ring transition-colors'>
+                  <DownloadIcon className='size-4'/> Download
+
+                </button>
 
               </div>
 
