8cb102107eb90d5b257f978c3efb5a7ee1587d67
KunalxKushwaha <kushwahakunal733@gmail.com>
2025-11-19 20:33:24 +0530

    Making AI Controllers for the functionality

--- Backend/controllers/aiController.js
+++ Backend/controllers/aiController.js
@@ -2,5 +2,60 @@
 // POST: /api/ai/enhance-pro-sum
 
 export const enhanceProfessionalSummary = async (req, res) => {
-    
+    try {
+        const {userContent} = req.body;
+
+        if(!userContent) {
+            return res.status(400).json({message: "Missing Required Fields"})
+        }
+
+        const response = await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+    messages: [
+        { role: "system", content: "You are an Expert in resume Writing. Your task is to enhance the professional summary of a resume.The Summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-Friendly. and only return text no options or anything else."},
+        {
+            role: "user",
+            content: userContent,
+        },
+    ],
+        })
+
+        const enhancedContent = response.choices[0].message.content;
+
+        return res.status(200).json({enhancedContent})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+
+}
+
+// Controller for enhancing a resume's job description
+// POST: /api/ai/enhance-job-desc
+
+export const enhanceJobDescription = async (req, res) => {
+    try {
+        const {userContent} = req.body;
+
+        if(!userContent) {
+            return res.status(400).json({message: "Missing Required Fields"})
+        }
+
+        const response = await ai.chat.completions.create({
+            model: process.env.OPENAI_MODEL,
+    messages: [
+        { role: "system", content: "You are an Expert in resume Writing. Your task is to enhance the professional summary of a resume.The Summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. Make it compelling and ATS-Friendly. and only return text no options or anything else."},
+        {
+            role: "user",
+            content: userContent,
+        },
+    ],
+        })
+
+        const enhancedContent = response.choices[0].message.content;
+
+        return res.status(200).json({enhancedContent})
+    } catch (error) {
+        return res.status(400).json({message: error.message})
+    }
+
 }
